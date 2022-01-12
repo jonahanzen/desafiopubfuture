9564fa4df56006e3ff2fa9e785656c67d4f77084
Jonny <jona_hanzen@hotmail.com>
2022-01-12 20:53:52 -0300

    Documentacao adicional dos controllers pelo swagger

--- src/main/java/br/com/despesa/DespesaController.java
+++ src/main/java/br/com/despesa/DespesaController.java
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.conta.dto.EditarContaDTO;
 import br.com.despesa.dto.EditarDespesaDTO;
 import br.com.despesa.dto.NovaDespesaDTO;
 import br.com.despesa.enums.TipoDespesa;
 import br.com.exception.ApiException;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("despesa")
@@ -30,16 +30,19 @@ public class DespesaController {
 	
 
 	@GetMapping
+	@ApiOperation(value = "Listar todas as despesas")
 	public List<Despesa> listarDespesas() {
 		return despesaService.listarReceitas();
 	}
 
 	@GetMapping("{despesaId}")
+	@ApiOperation(value = "Consultar dados de uma despesa pelo ID")
 	public Despesa dadosDespesaPorId(@PathVariable Long despesaId) throws ApiException {
 		return despesaService.dadosDespesa(despesaId);
 	}
 
 	@GetMapping("{contaId}/{dataInicio}/{dataFim}")
+	@ApiOperation(value = "Listar todas as despesas de uma contra entre um periodo e outro")
 	public List<Despesa> listarDespesasContaPorPeriodo(@PathVariable Long contaId,
 			@PathVariable("dataInicio") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicio,
 			@PathVariable("dataFim") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFim) {
@@ -47,6 +50,7 @@ public class DespesaController {
 	}
 	
 	@GetMapping("{dataInicio}/{dataFim}")
+	@ApiOperation(value = "Listar todas as despesas entre um periodo e outro")
 	public List<Despesa> listarDespesasPorPeriodo(
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicio,
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFim ) {
@@ -55,31 +59,37 @@ public class DespesaController {
 	
 
 	@GetMapping("total")
+	@ApiOperation(value = "Listar valor total de todas as despesas")
 	public Optional<Double> valorTotalDespesas() {
 		return despesaService.valorTotalDespesas();
 	}
 
 	@GetMapping("{contaId}/total")
+	@ApiOperation(value = "Listar todas as despesas de uma conta")
 	public Optional<Double> valorTotalDespesasPorContaId(@PathVariable Long contaId) {
 		return despesaService.valorTotalDespesaPorContaId(contaId);
 	}
 
 	@GetMapping("tipo/{tipoDespesa}")
+	@ApiOperation(value = "Listar todas as despesas pelo tipo da despesa")
 	public List<Despesa> despesaPorTipoDespesa(@PathVariable TipoDespesa tipoDespesa) {
 		return despesaService.dadosDespesaPorTipoDespesa(tipoDespesa);
 	}
 
 	@PostMapping
+	@ApiOperation(value = "Cadastrar uma nova despesa")
 	public Despesa cadastrarDespesa(@RequestBody NovaDespesaDTO despesaDTO) throws ApiException {
 		return despesaService.cadastrarDespesa(despesaDTO);
 	}
 
 	@PutMapping("{despesaId}")
+	@ApiOperation(value = "Editar uma despesa ja existente")
 	public void editarDespesa(@PathVariable Long despesaId, @RequestBody EditarDespesaDTO despesaDTO) throws ApiException {
 		despesaService.editarDespesa(despesaId, despesaDTO);
 	}
 
 	@DeleteMapping("{despesaId}")
+	@ApiOperation(value = "Remover uma despesa")
 	public void removerDespesa(@PathVariable Long despesaId) throws ApiException {
 		despesaService.removerDespesa(despesaId);
 	}
--- src/main/java/br/com/receita/ReceitaController.java
+++ src/main/java/br/com/receita/ReceitaController.java
@@ -20,7 +20,7 @@ import br.com.receita.dto.EditarReceitaDTO;
 import br.com.receita.dto.NovaReceitaDTO;
 import br.com.receita.enums.TipoReceita;
 import br.com.receita.exception.ReceitaNaoEncontradaException;
-import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("receita")
@@ -35,11 +35,13 @@ public class ReceitaController {
 	}
 
 	@GetMapping("{receitaId}")
+	@ApiOperation(value = "Listar dados de uma receita por ID")
 	public Receita dadosReceitaPorId(@PathVariable Long receitaId) throws ApiException {
 		return receitaService.dadosReceita(receitaId);
 	}
 
 	@GetMapping("{contaId}/{dataInicio}/{dataFim}")
+	@ApiOperation(value = "Listar todas as receitas de uma conta entre um periodo e outro")
 	public List<Receita> listarReceitasContaPorPeriodo(@PathVariable Long contaId,
 			@PathVariable("dataInicio") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicio,
 			@PathVariable("dataFim") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFim) {
@@ -47,6 +49,7 @@ public class ReceitaController {
 	}
 	
 	@GetMapping("{dataInicio}/{dataFim}")
+	@ApiOperation(value = "Listar todas as receitas entre um periodo e outro")
 	public List<Receita> listarReceitasPorPeriodo(
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataInicio,
 			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dataFim) {
@@ -54,31 +57,37 @@ public class ReceitaController {
 	}
 
 	@GetMapping("total")
+	@ApiOperation(value = "Listar o valor total de todas as receitas")
 	public Optional<Double> valorTotalReceitas() {
 		return receitaService.valorTotalReceitas();
 	}
 
 	@GetMapping("{contaId}/total")
+	@ApiOperation(value = "Listar o valor total de todas as receitas de uma conta")
 	public Optional<Double> valorTotalReceitasPorContaId(@PathVariable Long contaId) {
 		return receitaService.valorTotalReceitaPorContaId(contaId);
 	}
 
 	@GetMapping("tipo/{tipoReceita}")
+	@ApiOperation(value = "Listar todas as receitas pelo tipo da receita")
 	public List<Receita> receitasPorTipoReceita(@PathVariable TipoReceita tipoReceita) {
 		return receitaService.dadosReceitaPorTipoReceita(tipoReceita);
 	}
 	
 	@PostMapping
+	@ApiOperation(value = "Cadastrar uma nova receita")
 	public Receita cadastrarReceita(@RequestBody NovaReceitaDTO receitaDTO) throws ApiException {
 		return receitaService.cadastrarReceita(receitaDTO);
 	}
 
 	@PutMapping("{receitaId}")
+	@ApiOperation(value = "Editar uma receita que ja existe")
 	public void editarReceita(@PathVariable Long receitaId, @RequestBody EditarReceitaDTO receitaDTO) throws ApiException {
 		receitaService.editarReceita(receitaId, receitaDTO);
 	}
 
 	@DeleteMapping("{receitaId}")
+	@ApiOperation(value = "Remover uma receita")
 	public void removerReceita(@PathVariable Long receitaId) throws ReceitaNaoEncontradaException {
 		receitaService.removerReceita(receitaId);
 	}
